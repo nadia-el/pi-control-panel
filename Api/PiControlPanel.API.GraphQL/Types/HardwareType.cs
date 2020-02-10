60d87781a182c46d8f535aa7528295ed70d5c474
rembertmagri <rembert.magri@gmail.com>
2020-02-09 19:02:51 -0500

    Fixing case of Api folder

---
+++ Api/PiControlPanel.API.GraphQL/Types/HardwareType.cs
@@ -0,0 +1,24 @@
+﻿namespace PiControlPanel.Api.GraphQL.Types
+{
+    using global::GraphQL.Types;
+    using NLog;
+    using PiControlPanel.Api.GraphQL.Extensions;
+    using PiControlPanel.Domain.Contracts.Application;
+
+    public class HardwareType : ObjectGraphType
+    {
+        public HardwareType(IControlPanelService controlPanelService, ILogger logger)
+        {
+            Field<CpuType>()
+                .Name("Cpu")
+                .ResolveAsync(async context =>
+                {
+                    logger.Info("Cpu type");
+                    GraphQLUserContext graphQLUserContext = context.UserContext as GraphQLUserContext;
+                    var businessContext = graphQLUserContext.GetBusinessContext();
+
+                    return await controlPanelService.GetCpuAsync(businessContext);
+                });
+        }
+    }
+}
