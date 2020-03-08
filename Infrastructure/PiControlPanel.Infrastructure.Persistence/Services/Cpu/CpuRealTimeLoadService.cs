d2cf1e9e0c24e73254f055c45d8b9f2397cb1075
rembertmagri <rembert.magri@gmail.com>
2020-03-07 19:57:21 -0500

    Workaround for Dataloader issue

--- Infrastructure/PiControlPanel.Infrastructure.Persistence/Services/Cpu/CpuRealTimeLoadService.cs
+++ Infrastructure/PiControlPanel.Infrastructure.Persistence/Services/Cpu/CpuRealTimeLoadService.cs
@@ -2,10 +2,12 @@
 {
     using AutoMapper;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using NLog;
     using PiControlPanel.Domain.Contracts.Infrastructure.Persistence.Cpu;
     using PiControlPanel.Domain.Models.Hardware.Cpu;
     using PiControlPanel.Infrastructure.Persistence.Contracts.Repositories;
+    using PiControlPanel.Infrastructure.Persistence.Repositories;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -15,18 +17,39 @@
         BaseTimedService<CpuRealTimeLoad, Entities.Cpu.CpuRealTimeLoad>,
         ICpuRealTimeLoadService
     {
-        public CpuRealTimeLoadService(IUnitOfWork unitOfWork, IMapper mapper, ILogger logger)
+        private readonly IConfiguration configuration;
+
+        public CpuRealTimeLoadService(IConfiguration configuration, IUnitOfWork unitOfWork,
+            IMapper mapper, ILogger logger)
             : base(unitOfWork, mapper, logger)
         {
+            this.configuration = configuration;
             this.repository = unitOfWork.CpuRealTimeLoadRepository;
         }
 
+        /// <summary>
+        ///     Gets the RealTimeLoads from the list of datetimes
+        /// </summary>
+        /// <remarks>
+        ///     The unit of work is being created manually instead of relying on the dependency injection
+        ///     lifetime due to existing issues with GraphQL dataloader.
+        ///     https://github.com/graphql-dotnet/graphql-dotnet/pull/1511
+        ///     https://github.com/graphql-dotnet/graphql-dotnet/issues/1310
+        ///     Once they are addressed, this can be changed to get the unit of work directly in the
+        ///     constructor via dependency injection
+        /// </remarks>
+        /// <param name="dateTimes">List of datetimes for which to fetch the RealTimeLoads</param>
+        /// <returns>A dictionary containg the datetimes as keys and the RealTimeLoads as values</returns>
         public async Task<IDictionary<DateTime, CpuRealTimeLoad>> GetRealTimeLoadsAsync(
             IEnumerable<DateTime> dateTimes)
         {
-            var entities = await this.repository.GetMany(l => dateTimes.Contains(l.DateTime))
-                .ToDictionaryAsync(i => i.DateTime, i => i);
-            return mapper.Map<Dictionary<DateTime, CpuRealTimeLoad>>(entities);
+            using (var unitOfWork = new UnitOfWork(this.configuration, this.logger))
+            {
+                var repository = unitOfWork.CpuRealTimeLoadRepository;
+                var entities = await repository.GetMany(l => dateTimes.Contains(l.DateTime))
+                    .ToDictionaryAsync(i => i.DateTime, i => i);
+                return mapper.Map<Dictionary<DateTime, CpuRealTimeLoad>>(entities);
+            }
         }
     }
 }
